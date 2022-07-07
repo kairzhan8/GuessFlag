38c2d4dfbf71b41030b605f1232c0bbf366fcd6b
kairzhan <kkural@dar.io>
2022-07-07 15:55:19 +0600

    Initial Commit

---
+++ GuestFlag/GuestFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestFlagApp.swift
+//  GuestFlag
+//
+//  Created by Kaiyrzhan Kural on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuestFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
